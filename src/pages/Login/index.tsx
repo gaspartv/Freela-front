4338d0a4912b0f9e3baef92ea3c27fda0079a78f
Giancarlo <giancarlopessatti@hotmail.com>
2022-11-02 11:58:32 -0300

    Card_N-1 Login/Release: Rework

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,34 @@
 
 import { SpanTop } from "../../components/SpanTop/style";
 import { LoginStyled } from "./styles";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+// import { AuthContext } from "../../contexts/AuthContext";
+
+// Deixei comentado as coisas que eu não sei o nome ou não foram feitas ainda!
+// a mensagem de erro está comentada pois estava dando problema, caso alguem saiba resolver, pode dale!
 
 export const Login = () => {
+
+  // const { LoginUser } = useContext(AuthContext);
+
+  const formSchema = yup.object().shape({
+    password: yup.string().required("Senha obrigatório"),
+    email: yup.string().required("Nome obrigatório").email("Email inválido"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
+
+
+
+
   return (
 
 
@@ -16,15 +42,17 @@ export const Login = () => {
           <div className="SideDesktop"></div>
           <div className="Content">
             <h1>Login</h1>
-            <form>
+            <form onSubmit={handleSubmit(LoginUser)}>
+              <>
               <h2>E-mail</h2>
-              <input type="email" placeholder="Digite seu E-mail" />
-              <p>erro</p>
+              <input  {...register("email")} type="email" placeholder="Digite seu E-mail" />
+              {/* <p>{errors.email?.message}</p> */}
               <h2>Senha</h2>
-              <input type="email" placeholder="Digite seu E-mail" />
-              <p>erro</p>
+              <input {...register("password")} type="password" placeholder="Digite seu E-mail" />
+              {/* <p>{errors.password?.message}</p> */}
               <span>Ainda não possui cadastro?</span>
-              <button>Entrar</button>
+              <button type="submit">Entrar</button>
+              </>
             </form>
           </div>
         </section>
