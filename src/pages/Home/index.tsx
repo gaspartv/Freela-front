0a4644a6cb8f72777b6a2b4a3d5ba21cc3eec7a5
= <cadastro@diegogaspar.dev.br>
2022-11-02 15:17:23 -0300

    fix: error merge

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,5 @@
 import { HomeStyled } from "./styles";
 
 export const Home = () => {
-<<<<<<< HEAD
   return <HomeStyled><h1>teste home</h1></HomeStyled>;
-=======
-  return <HomeStyled>
-    <h1>Home</h1>
-  </HomeStyled>;
->>>>>>> 3e33150ba286725d9f0749dbe6708dbc45723e24
 };
