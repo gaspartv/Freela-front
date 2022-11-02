21319355f1df94073fb93036f3e6ba0bafad7a45
Giancarlo <giancarlopessatti@hotmail.com>
2022-11-02 11:38:20 -0300

    Card_N-1 Login/HotFix: Rework

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -11,8 +11,23 @@ export const Login = () => {
 
       <SpanTop/>
       <main>
-
-        
+        <div className="Logimg"><img src="" alt="" /></div>
+        <section>
+          <div className="SideDesktop"></div>
+          <div className="Content">
+            <h1>Login</h1>
+            <form>
+              <h2>E-mail</h2>
+              <input type="email" placeholder="Digite seu E-mail" />
+              <p>erro</p>
+              <h2>Senha</h2>
+              <input type="email" placeholder="Digite seu E-mail" />
+              <p>erro</p>
+              <span>Ainda não possui cadastro?</span>
+              <button>Entrar</button>
+            </form>
+          </div>
+        </section>
       </main>
     
   </LoginStyled>
