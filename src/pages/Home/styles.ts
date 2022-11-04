6de06ce374866d9cadaf3f251d36647c1f4176b6
daniel delos silva <apeiron52@gmail.com>
2022-11-04 16:28:02 -0300

    Card_N-3 feat/Home: estilização cards multiplos page home

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -8,18 +8,42 @@ export const ContainerHome = styled.div`
 `;
 
 export const HomeStyled = styled.div`
+  /* border: solid red; */
   color: black;
   display: flex;
   justify-content: center;
   align-items: center;
   margin-top: 20px;
+  padding: 6px 0px;
+
   ul {
-    border: solid;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    gap: 10px;
   }
+
   li {
     display: flex;
     flex-direction: column;
     border: solid;
+    gap: 10px;
+    border: 1px solid #177369;
+    border-radius: 8px;
+    padding: 14px;
+    height: 170px;
+    width: 400px;
+    border-radius: 25px;
+  }
+
+  .cardTitle {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .cardDescription {
+    display: flex;
+    flex-direction: column;
   }
 `;
 
@@ -34,24 +58,33 @@ export const HeaderHome = styled.header`
 `;
 
 export const HomeTitle = styled.div`
-  /* border: solid; */
+  /* border: solid red; */
   display: flex;
 
-  div{
+  div {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
   }
   span {
-    color: green;
+    color: var(--Primary);
   }
 
-  h1{
+  h1 {
     font-size: 48px;
   }
 
-  p{
-    font-size:24px;
+  p {
+    font-size: 24px;
   }
+`;
+
+export const HeaderLine = styled.div`
+  background-image: linear-gradient(
+    to bottom,
+    var(--Primary) 20px,
+    var(--Secondary) 0
+  );
+  height: 20px;
 `;
