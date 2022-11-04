8a751252ad58a62dabf77ec7c8f871f36ce1acdf
daniel delos silva <apeiron52@gmail.com>
2022-11-04 18:50:10 -0300

    Card_N-3 feat/Home: resolvendo conflito developer

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,11 @@ import { ToastContainer } from "react-toastify";
 import RouterMain from "./routes";
 import GlobalStyle from "./styles/Global";
 import "react-toastify/dist/ReactToastify.css";
-<<<<<<< HEAD
-import { SpanTop } from "./components/SpanTop/style";
-=======
 import { ChakraProvider } from "@chakra-ui/react";
->>>>>>> feat/home
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <GlobalStyle />
-      <ToastContainer
-        position="top-right"
-        autoClose={1500}
-        theme="colored"
-        pauseOnHover={true}
-      />
-      <SpanTop />
-      <RouterMain />
-=======
       <ChakraProvider>
         <GlobalStyle />
         <ToastContainer
@@ -32,7 +17,6 @@ function App() {
         />
         <RouterMain />
       </ChakraProvider>
->>>>>>> feat/home
     </>
   );
 }
