0f835759b642529ececaeb4cf5af9860c690b6d1
= <cadastro@diegogaspar.dev.br>
2022-11-07 16:08:32 -0300

    Fix Page Error Mobile

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ import SettingProvider from "./contexts/SettingContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
-);
+); 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
